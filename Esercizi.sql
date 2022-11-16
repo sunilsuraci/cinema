8b3e944b192f54048a4e7218b50539cb927ad5db
sunilsuraci <suracisunil@gmail.com>
2022-11-16 17:55:10 +0100

    Es N4

--- Esercizi.sql
+++ Esercizi.sql
@@ -9,3 +9,13 @@ where sale.citta= 'Pisa'
 SELECT film.regista, film.annoproduzione
 FROM film
 WHERE film.regista like 'Makoto%' and film.annoproduzione > 1960
+
+--N3 Il titolo e la durta dei film di fantascienza giapponesi o francesi prodotti dopo il 1990
+
+
+
+--N4 il titolo dei film di fantascienza giapponesi prodotti dopo il 1990 oppure francesi
+
+SELECT film.genere, film.nazionalita, film.titolo
+FROM film
+WHERE film.annoproduzione > 1990  and film.genere like 'drammatico' and (film.nazionalita like 'USA' or film.nazionalita like 'danese')
