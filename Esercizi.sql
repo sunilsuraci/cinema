fd7d737e6408386df7039c42967f1cee84f8d44e
sunilsuraci <suracisunil@gmail.com>
2022-11-16 17:38:35 +0100

    Es. N2

--- Esercizi.sql
+++ Esercizi.sql
@@ -3,3 +3,9 @@
 select sale.nome
 from sale
 where sale.citta= 'Pisa'
+
+--N2 I film di Fellini prodotti dopo il 1960
+
+SELECT film.regista, film.annoproduzione
+FROM film
+WHERE film.regista like 'Makoto%' and film.annoproduzione > 1960
